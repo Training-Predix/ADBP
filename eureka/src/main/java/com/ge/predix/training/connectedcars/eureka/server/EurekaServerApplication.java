1222cc00c4dfa60cc5363a36f31369c15d866234
LOGON\502706889 <sharat.tiruveedhula@ge.com>
2018-01-27 20:34:14 -0800

    added eureka server app

---
+++ eureka/src/main/java/com/ge/predix/training/connectedcars/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ge.predix.training.connectedcars.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
